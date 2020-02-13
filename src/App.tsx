cad4b6581434b00ac28d8c6e0b475857e039b8c2
Sebastian <avaven@gmail.com>
2020-02-13 17:26:30 +0100

    Add data fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { fetchData } from './utils/api';
 
 const App = () => {
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return <div></div>;
 };
 
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,1 +1,27 @@
-export const fetchData = () => {};
+import csv from 'csvtojson/v2';
+
+export interface Row {
+  Date: string;
+  Datasource: string;
+  Campaign: string;
+  Clicks: number;
+  Impressions: number;
+}
+
+const URL = 'http://adverity-challenge.s3-website-eu-west-1.amazonaws.com/DAMKBAoDBwoDBAkOBAYFCw.csv';
+
+const csvToJson = (data: string) => {
+  return csv({
+    colParser: {
+      Impressions: 'number',
+      Clicks: 'number',
+      Date: 'string',
+    },
+  }).fromString(data);
+};
+
+export const fetchData = async () => {
+  const res = await fetch(URL);
+  const data = await res.text();
+  return csvToJson(data);
+};
