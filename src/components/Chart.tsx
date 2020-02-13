d2283a54d8bf5559ba4e4bba16b4c0b0eb65d7d3
Sebastian <avaven@gmail.com>
2020-02-13 17:17:44 +0100

    Add basic structure

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const App = () => {
+const Chart = () => {
   return <div></div>;
 };
 
-export default App;
+export default Chart;
--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const App = () => {
+const Filters = () => {
   return <div></div>;
 };
 
-export default App;
+export default Filters;
