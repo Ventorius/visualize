ede840a9bd51adfe53ad3cbf3af021e8ca41e6bb
Sebastian <avaven@gmail.com>
2020-02-13 17:29:36 +0100

    Add basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import React, { useEffect } from 'react';
 import { fetchData } from './utils/api';
+import styled from 'styled-components';
+
+import Filters from './components/Filters';
+import Chart from './components/Chart';
+
+const Wrapper = styled.div`
+  height: 800px;
+  display: flex;
+`;
+
+const ChartWrapper = styled.div`
+  width: 75%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const App = () => {
   useEffect(() => {
     fetchData();
   }, []);
 
-  return <div></div>;
+  return (
+    <Wrapper>
+      <Filters />
+      <ChartWrapper>
+        <Chart />
+      </ChartWrapper>
+    </Wrapper>
+  );
 };
 
 export default App;
--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 25%;
+  display: flex;
+  align-items: center;
+  padding: 100px 20px 20px 20px;
+  flex-direction: column;
+  background-color: red;
+`;
 
 const Filters = () => {
-  return <div></div>;
+  return <Wrapper></Wrapper>;
 };
 
 export default Filters;
